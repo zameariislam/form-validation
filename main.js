c87eba7213827a1e2159260f2a3cebe13d64de33
zameariislam <zameariislam@gmail.com>
2022-06-09 12:26:04 +0200

    form validation finished

--- main.js
+++ main.js
@@ -7,10 +7,39 @@ let classes = (classes) => document.getElementsByClassName(classes)
 let username = id('username');
 let email = id('email');
 let password = id('password');
+let form = id('form')
 let errorMessage = document.getElementsByClassName('error')
 let successIcon = classes('success-icon')
 let failureIcon = classes('failure-icon')
-console.log(failureIcon)
+// add event listener 
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    engine(username, 0, 'User name can not be blank');
+    engine(email, 1, 'Email  can not be blank');
+    engine(password, 2, 'Password can not be blank');
+
+
+})
+
+let engine = (id, serial, message) => {
+
+    if (id.value.trim() === '') {
+        errorMessage[serial].innerHTML = message;
+        failureIcon[serial].style.opacity = 1
+        successIcon[serial].style.opacity = 0
+    }
+    else {
+        errorMessage[serial].innerHTML = ''
+        successIcon[serial].style.opacity = 1
+        failureIcon[serial].style.opacity = 0
+
+    }
+
+    id.value = ''
+
+}
+
+
 
 
 
