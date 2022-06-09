650326cf526f3762ca16871bff03a022389e26b7
zameariislam <zameariislam@gmail.com>
2022-06-09 11:45:53 +0200

    target different input

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+// target id and classes 
+
+let id = (id) => document.getElementById(id)
+let classes = (classes) => document.getElementsByClassName(classes)
+let username = id('username');
+let email = id('email');
+let password = id('password');
+let errorMessage = document.getElementsByClassName('error')
+let successIcon = classes('success-icon')
+let failureIcon = classes('failure-icon')
+console.log(failureIcon)
+
+
+
